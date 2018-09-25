3d11f09b1125f6fcd064c830dbed2c440e7498ec
R. Michael Cazad <michaelcazad@gmail.com>
2018-09-25 19:14:07 +0000

    First logic - get command line argument and error out with message if there is none. Also turn argc into int type and store in variable k

--- caesar.c
+++ caesar.c
@@ -1,11 +1,11 @@
 /*
  * Program: Caesar
  * Description: Program will get a key (integer) from the user and rotate each character of a plaintext
- *              message by that amount, wrapping around. Capitals should remain, as lowercase remains 
+ *              message by that amount, wrapping around. Capitals should remain, as lowercase remains
  *              and punctuation.
  *
- * Usage Instructions: 
- *                      
+ * Usage Instructions:
+ *
  *
  * Usage Examples:
  *
@@ -21,5 +21,15 @@
 int main(int argc, string argv[])
 
 {
-
+    if (argc == 2)
+    {
+        int k = atoi(argv[1]);
+        printf("key is: %i\n", k);
+    }
+    else
+    {
+        printf("No command argument given.\n");
+        return 1;
+    }
+    eprintf("out of if statement\n");
 }
